b3fc1ac1ad5e38814ff8c049fb50efdd82ffb8a9
willholmeswastaken <willholmes97@icloud.com>
2020-12-27 21:26:43 +0000

    Icon changes

--- src/components/icon.js
+++ src/components/icon.js
@@ -9,26 +9,7 @@ const StyledIcon = styled.img`
   cursor: ${props => (props.clickable ? 'pointer' : 'auto')};
 `;
 
-const StyledSvgIcon = styled.img`
-  display: block;
-  margin: ${props => (props.margin ? props.margin : '0 auto 5px auto')};
-  background-size: ${props => props.height} ${props => props.width};
-  cursor: ${props => (props.clickable ? 'pointer' : 'auto')};
-`;
-
 const Icon = props => (
-    props.src.includes('.svg')
-        ?
-        <StyledSvgIcon
-            margin={props.margin}
-            src={props.src}
-            height={props.height}
-            width={props.width}
-            clickable={props.clickable}
-            onClick={props.clicked}
-            alt={props.alt}
-        />
-        :
         <StyledIcon
             margin={props.margin}
             src={props.src}
