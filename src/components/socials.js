b096f72a8f4a69da7d3864f4cbfc4ed8182116de
Will Holmes <willholmes97@icloud.com>
2022-04-18 12:11:53 +0100

    Updated home page and added all my new projects

--- src/components/socials.js
+++ src/components/socials.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import styled from 'styled-components';
-import Icon from './icon';
+import React from "react"
+import styled from "styled-components"
+import Icon from "./icon"
 
 const StyledSocials = styled.div`
-    display: flex;
-    margin-left:8px;
-    img {
-        cursor:pointer;
-    }
+  display: flex;
+  gap:20px;
+  margin-left: 8px;
+  img {
+    cursor: pointer;
+  }
+`;
+const StyledLogo = styled.div`
+  font-size: 2rem;
+  font-weight: 900;
+  color: black;
+  display: block;
+  cursor:pointer !important;
+  margin: ${(props) => (props.margin ? props.margin : '5px auto 16px auto')};
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  cursor: ${(props) => (props.clickable ? 'pointer' : 'auto')};
 `;
 
 const Socials = () => (
@@ -19,9 +31,13 @@ const Socials = () => (
           <Icon alt="blog" src="/dev-to.svg" />
           <b>&nbsp;Blog</b>
         </a>
+        <a href="/projects">
+          <StyledLogo>wh.</StyledLogo>
+          <b>&nbsp;Projects</b>
+        </a>
       </>
     </StyledSocials>
   </>
-);
+)
 
-export default Socials;
+export default Socials
