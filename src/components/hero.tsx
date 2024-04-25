5f21bb1b2f191bd0dd10a85c3525fdec5f52337a
Will Holmes <willholmes97@icloud.com>
2024-04-25 18:44:13 +0100

    changes to copy

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -37,7 +37,7 @@ export default function Hero() {
                     <div className="space-y-3 md:space-y-4 lg:space-y-6 col-span-2">
                         <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold tracking-tight">Will Holmes</h1>
                         <p className="text-gray-600 dark:text-gray-400 text-md sm:text-base md:text-lg lg:text-xl">
-                            I&apos;m a full-stack developer with a passion for building beautiful and functional web applications that scale.
+                            I&apos;m a full-stack developer with a passion for building scalable, reliable and functional web applications.
                         </p>
                     </div>
                 </div>
